2d39db05dffe32661e796b3a1a616f7c7186354b
Mr Shadab <mrshadab@MacBookAir.lan>
2026-05-18 19:31:00 +0530

    Fixed Vercel crash

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask, request, render_template_string
-import os
+from flask import Flask, render_template_string
 
 app = Flask(__name__)
-
-UPLOAD_FOLDER = "uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 HTML = """
 <!DOCTYPE html>
@@ -16,40 +12,12 @@ HTML = """
 
     <h1>PDF Malware Analysis Toolkit</h1>
 
-    <form method="POST" enctype="multipart/form-data">
-        <input type="file" name="pdf_file">
-        <br><br>
-        <button type="submit">Upload & Scan</button>
-    </form>
-
-    {% if filename %}
-        <h3>Uploaded File:</h3>
-        <p>{{ filename }}</p>
-
-        <h3>Scan Result:</h3>
-        <p>No malware detected (Demo Mode)</p>
-    {% endif %}
+    <h3>Deployment Successful ✅</h3>
 
 </body>
 </html>
 """
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
-
-    filename = None
-
-    if request.method == "POST":
-
-        file = request.files["pdf_file"]
-
-        if file:
-            filepath = os.path.join(UPLOAD_FOLDER, file.filename)
-            file.save(filepath)
-
-            filename = file.filename
-
-    return render_template_string(HTML, filename=filename)
-
-if __name__ == "__main__":
-    app.run()
+    return render_template_string(HTML)
